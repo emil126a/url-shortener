a6815e96bba233c7d28981dd37327d51c46b5028
Emil Azizov <azizov.emil@gmail.com>
2025-03-12 19:19:41 +0100

    Basic implementation of url shortener

---
+++ demo/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String BASE_URL = "https://short.url/";
+
+    public static void main(String[] args) {
+
+        String longUrl = shortenUrl(BASE_URL + "slkdksdl/lksklk/sdlkdslkld");
+        System.out.println(BASE_URL + "" + longUrl);
+    }
+
+
+    public static String shortenUrl(String longUrl) {
+        int hashCode = longUrl.hashCode();
+        int positiveHash = Math.abs(hashCode);
+        return encodedBase62(positiveHash);
+    }
+
+    public static String encodedBase62(int number) {
+        StringBuilder sb = new StringBuilder();
+
+        do {
+            sb.append(CHARACTERS.charAt((int) (number % CHARACTERS.length())));
+            number = number / CHARACTERS.length();
+        } while (number > 0);
+
+
+        return sb.toString();
+    }
+}
